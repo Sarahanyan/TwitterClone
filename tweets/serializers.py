9d103a0b8e7ba4e46426781f7ee1a5e5647ea2c8
Sarahanyan <saranyango@gmail.com>
2020-12-20 01:27:21 +0300

    retweet logic in django
    Uses created serialization of parent tweet of a retweet to set content of the retweet

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -36,17 +36,12 @@ class TweetCreateSerializer(serializers.ModelSerializer):
 
 class TweetSerializer(serializers.ModelSerializer):
     likes = serializers.SerializerMethodField(read_only=True)
-    content = serializers.SerializerMethodField(read_only=True)
+    # og_tweet = TweetCreateSerializer(source="parent", read_only=True)
+    parent = TweetCreateSerializer(read_only=True)
 
     class Meta:
         model = Tweet
-        fields = ["id", "content", "likes", "is_retweet"]
+        fields = ["id", "content", "likes", "is_retweet", "parent"]
 
     def get_likes(self, obj):
         return obj.likes.count()
-
-    def get_content(self, obj):
-        content = obj.content
-        if obj.is_retweet:
-            content = obj.parent.content
-        return content
