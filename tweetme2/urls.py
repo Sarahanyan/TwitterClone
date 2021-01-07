8969b340c16f87376cfb618142bd1ba63e54511e
Sarahanyan <saranyango@gmail.com>
2021-01-07 10:02:26 +0300

    Added user authentication along with its  appropriate views in Django

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -19,6 +19,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
+from accounts.views import (
+    login_view,
+    logout_view,
+    register_view)
+
 from tweets.views import (
     home_view,
     tweets_list_view,
@@ -29,8 +34,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path("", home_view),
     path("", tweets_list_view),
+    path("login/", login_view),
+    path("logout/", logout_view),
+    path("register/", register_view),
     path("<int:tweet_id>", tweets_detail_view),
-    path("<str:username>", tweets_profile_view),
+    path("profile/<str:username>", tweets_profile_view),
     path("react/", TemplateView.as_view(template_name="react_via_dj.html")),
 
     path("api/tweets/", include("tweets.api.urls"))
