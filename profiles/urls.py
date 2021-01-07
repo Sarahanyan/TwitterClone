3ca937e9a5c5c6961ab03e9e382abc4962599b0f
Sarahanyan <saranyango@gmail.com>
2021-01-07 14:31:54 +0300

    View and url to update profile added

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from .views import tweets_profile_view
+from .views import tweets_profile_view, profile_update_view
 
 urlpatterns = [
+    path("edit", profile_update_view),
     path("<str:username>", tweets_profile_view),
+
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import Http404
 from .models import Profile
+from .forms import ProfileForm
+
+
+def profile_update_view(request, *args, **kwargs):
+    if not request.user.is_authenticated:
+        return redirect("/login?next=/profile/update")
+
+    user = request.user
+    user_data = {
+        "first_name": user.first_name,
+        "last_name": user.last_name,
+        "email": user.email
+    }
+    my_profile = user.profile
+    form = ProfileForm(request.POST or None,
+                       instance=my_profile, initial=user_data)
+
+    if form.is_valid():
+        profile_obj = form.save(commit=False)
+        user.first_name = form.cleaned_data.get("first_name")
+        user.last_name = form.cleaned_data.get("last_name")
+        user.email = form.cleaned_data.get("email")
+        user.save()
+        profile_obj.save()
+
+    context = {
+        "form": form,
+        "title": "Update Profile",
+        "btn_label": "Click to update"}
+
+    return render(request, "profiles/form.html", context)
 
 
 def tweets_profile_view(request, username, *args, **kwargs):
